af2ec9b99ea405d959c164c3326a8b34389cae4e
musond12 <musond12@gasos-ro.cz>
2025-03-25 08:19:10 +0100

    kokoko

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,44 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+
         System.out.print("Zadejte počet zařízení: ");
         int pocet = scanner.nextInt();
-        scanner.nextLine(); // pohlcení Enteru po zadání čísla
+        scanner.nextLine();
 
-        // Vytvoření pole pro zařízení
+
         IPrehravac[] zariadenia = new IPrehravac[pocet];
 
-        // Načítání jednotlivých zařízení
+
         for (int i = 0; i < pocet; i++) {
             System.out.print("Zadejte typ zařízení (MP3, Smartphone, Radio): ");
             String typ = scanner.nextLine();
 
-            if (typ.equalsIgnoreCase("MP3")) {
-                System.out.print("Zadejte značku MP3: ");
-                String znacka = scanner.nextLine();
-                zariadenia[i] = new MP3(znacka);
-            } else if (typ.equalsIgnoreCase("Smartphone")) {
-                System.out.print("Zadejte model mobilu: ");
-                String model = scanner.nextLine();
-                zariadenia[i] = new Smartphone(model);
-            } else if (typ.equalsIgnoreCase("Radio")) {
-                zariadenia[i] = new Radio();
-            } else {
-                System.out.println("Neplatný typ zařízení.");
+            switch (typ.toLowerCase()) {
+                case "mp3":
+                    System.out.print("Zadejte značku MP3: ");
+                    zariadenia[i] = new MP3(scanner.nextLine());
+                    break;
+                case "smartphone":
+                    System.out.print("Zadejte model mobilu: ");
+                    zariadenia[i] = new Smartphone(scanner.nextLine());
+                    break;
+                case "radio":
+                    zariadenia[i] = new Radio();
+                    break;
+                default:
+                    System.out.println("Neplatný typ zařízení.");
+                    i--;
+                    break;
             }
         }
 
-        // Menu pro práci s zařízeními
+
         while (true) {
-            System.out.println("\n--- Menu ---");
+            System.out.println("--- Menu ---");
             System.out.println("1. Zobrazit všechna zařízení");
             System.out.println("2. Přehrát zařízení");
             System.out.println("3. Zastavit přehrávání");
@@ -40,40 +46,28 @@ public class Main {
             System.out.println("5. Konec");
             System.out.print("Zvolte akci: ");
             int volba = scanner.nextInt();
-            scanner.nextLine(); // pohlcení Enteru po zadání čísla
+            scanner.nextLine();
 
             switch (volba) {
                 case 1:
-                    // Zobrazí všechna zařízení
                     for (int i = 0; i < zariadenia.length; i++) {
                         System.out.println(i + ": " + zariadenia[i]);
                     }
                     break;
                 case 2:
-                    // Přehrání zařízení
-                    System.out.print("Zadejte index zařízení pro přehrání: ");
-                    int indexPrehrani = scanner.nextInt();
-                    if (indexPrehrani >= 0 && indexPrehrani < zariadenia.length) {
-                        zariadenia[indexPrehrani].prehraj(true);
-                    } else {
-                        System.out.println("Neplatný index.");
-                    }
-                    break;
                 case 3:
-                    // Zastavení přehrávání
-                    System.out.print("Zadejte index zařízení pro zastavení: ");
-                    int indexZastaveni = scanner.nextInt();
-                    if (indexZastaveni >= 0 && indexZastaveni < zariadenia.length) {
-                        zariadenia[indexZastaveni].prehraj(false);
+                    System.out.print("Zadejte index zařízení: ");
+                    int index = scanner.nextInt();
+                    if (index >= 0 && index < zariadenia.length) {
+                        zariadenia[index].prehraj(volba == 2);
                     } else {
                         System.out.println("Neplatný index.");
                     }
                     break;
                 case 4:
-                    // Přehrání písničky na zařízení
-                    System.out.print("Zadejte index zařízení pro přehrání písničky: ");
+                    System.out.print("Zadejte index zařízení: ");
                     int indexPisnicky = scanner.nextInt();
-                    scanner.nextLine(); // pohlcení Enteru
+                    scanner.nextLine();
                     if (indexPisnicky >= 0 && indexPisnicky < zariadenia.length) {
                         System.out.print("Zadejte název písničky: ");
                         String pisnicka = scanner.nextLine();
@@ -83,7 +77,6 @@ public class Main {
                     }
                     break;
                 case 5:
-                    // Konec programu
                     System.out.println("Konec programu.");
                     return;
                 default:
